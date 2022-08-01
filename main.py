cac5e4c633fa7f36231555c0911cfd9ab77e6a30
Ruth Hermosillo <hermosilloruth59@gmail.com>
2022-08-01 13:27:59 -0600

    Created Lists  for Destinations, Restaurants, Mode of Transportation, and Entertainment

---
+++ main.py
@@ -0,0 +1,4 @@
+destinations = ['Denver', 'Aspen', 'Boulder', 'Breckenridge', 'Vail', 'Canon City']
+restaurants = ['Buckhorn Exchange', 'Bingo Burger', 'Illegal Petes', 'Casa Bonita', 'Beau Jos', 'Biker Jims', 'Iron Chateau']
+transportation = ['Train', 'Car', 'Bicycle', 'RTD Rail', 'Uber']
+entertainment = ['History Colorado Center', 'Ute Indian Museum', 'Sunrise Ballon Ride', 'Cheyenne Mountain Zoo', 'Red Rock Canyon Concert', '3-Hour Brewery Tour']
