7a876fc2a5ac338670be88f0426c545b7c165b5b
Ruth Hermosillo <hermosilloruth59@gmail.com>
2022-08-01 13:52:34 -0600

    Defined the Function to Choose a Random Destination.

--- main.py
+++ main.py
@@ -2,3 +2,10 @@ destinations = ['Denver', 'Aspen', 'Boulder', 'Breckenridge', 'Vail', 'Canon Cit
 restaurants = ['Buckhorn Exchange', 'Bingo Burger', 'Illegal Petes', 'Casa Bonita', 'Beau Jos', 'Biker Jims', 'Iron Chateau']
 transportation = ['Train', 'Car', 'Bicycle', 'RTD Rail', 'Uber']
 entertainment = ['History Colorado Center', 'Ute Indian Museum', 'Sunrise Ballon Ride', 'Cheyenne Mountain Zoo', 'Red Rock Canyon Concert', '3-Hour Brewery Tour']
+
+import random
+
+def random_destination():
+    print ('Your destination is generating...')
+    print(random.choice(destinations))
+
