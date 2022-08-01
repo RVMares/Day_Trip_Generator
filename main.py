9abd77048f58fa563c72925f55654af6d9cef649
Ruth Hermosillo <hermosilloruth59@gmail.com>
2022-08-01 16:52:11 -0600

    Updated Destinations and List Options

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
-destinations = ['Denver', 'Aspen', 'Boulder', 'Breckenridge', 'Vail', 'Canon City']
-restaurants = ['Buckhorn Exchange', 'Bingo Burger', 'Illegal Petes', 'Casa Bonita', 'Beau Jos', 'Biker Jims', 'Iron Chateau']
-transportation = ['Train', 'Car', 'Bicycle', 'RTD Rail', 'Uber']
-entertainment = ['History Colorado Center', 'Ute Indian Museum', 'Sunrise Ballon Ride', 'Cheyenne Mountain Zoo', 'Red Rock Canyon Concert', '3-Hour Brewery Tour']
+destinations = ['Manitou Springs', 'Breckenridge', 'Denver']
+restaurants_Manitou_Springs = ['Iron Chateau', 'Border Burger Bar', 'Manitou Brewing Company', 'Mo\'s Diner & Lounge', 'The Mason Jar', 'Pizzeria Rustica']
+entertainment_Manitou_Springs = ['Santa\'s Workshop', 'River Running', 'Colorado Wolf and Wildlife Center', 'Garden of the Gods', 'The Incline', 'Western Museum of Mining and Industry', 'Miramont Castle']
+restaurants_Breckenridge = ['Hearthstone', 'Piante Pizzeria', 'Twist', 'Relish', 'Aurum Food and Wine', 'Mountain Flying Fish']
+entertainment_Breckenridge = ['Breckenridge Ski Resort', 'Rafting', 'Shopping on Main Street', 'Country Boy Mine', 'Visit Isak the Breckenridge Troll', 'Edwin Carter Museum and Park', 'Breckenridge Theater presenting "\The Play That Goes Wrong\"']
+restaurants_Denver = ['The Poke House', 'Apple Blossom', 'Chez Maggy', 'A5 Steakhouse', 'Toro Latin Kitchen', 'Cantina Loca', 'Glo Noodle House', 'Jax Fish House']
+entertainment_Denver = ['16th Street Mall', 'Larimer Square', 'Meow Wolf Denver', 'Colorado Railroad Museum', 'Elitch Gardens Theme and Water Park', 'Denver Zoo', 'Denver Museum of Nature and Science', 'Jurassic World: The Exhibition']
+transportation = ['Train', 'Rental Car', 'Bicycle', 'RTD Rail', 'Uber', 'Taxi', 'Rental Scooter']
 
 import random
 
